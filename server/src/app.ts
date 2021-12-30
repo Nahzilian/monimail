6a639ee4a6739c2cd5d0d1e9e94b762278e68dab
Jayce Soul <xxJayceSoulxx@gmail.com>
2021-12-29 22:34:54 -0500

    redis connection (WIP: ECONNREFUSED error)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,33 @@
-import express from 'express'
-import configs from './configs/default'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { createClient } from 'redis';
+
+dotenv.config();
+
+(async () => {
+  const client = createClient();
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  await client.ping();
+
+  await client.set('key', 'value');
+  const value = await client.get('key');
+})();
+
+
 
 const app = express();
 const port = 3000;
 
-configs()
 
 app.get('/', (_, res) => {
-  res.send("Hello world");
+  res.send(`Hello World! my DB name is ${process.env.DB_HOST}`);
 });
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
