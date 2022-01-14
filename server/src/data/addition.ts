94b13a25380b26244a609d1a32b3a8af5ffe63f2
Nahzilian <nahz3112@gmail.com>
2022-01-13 21:30:29 -0500

    Fixed problem

--- server/src/data/addition.ts
+++ server/src/data/addition.ts
@@ -1,12 +1,10 @@
 import { mysql } from '../app'
 
 export const createNewUser = async (id: string, firstname: string, lastname: string, email:string, username:string, password:string, role:string, company:string, callback: Function): Promise<any> => {
-    const insert = "INSERT INTO users VALUES (?,?,?,?,?,?,?,?,'no')";
-    let sqlport = require('mysql');
-    const query = sqlport.format(insert, [id,firstname, lastname, email, username, password, role, company])
+    const query = `insert into users (id,first_name,last_name,email,username,password,role,company_id,verified)
+    VALUES ("${id}","${firstname}", "${lastname}", "${email}", "${username}", "${password}", "${role}", "${company}", "no");`;
     mysql.db.query(query, (err, results) => {
         if (err) throw err
-        callback();
+        callback(results);
     })
-    callback();
 }
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,6 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import { getUserById, getUserByUsername, emailInUse } from '../data/query';
-import {isPasswordCorrect, encryptPassword} from '../modules/authentication';
+import { isPasswordCorrect, encryptPassword } from '../modules/authentication';
 import { createNewUser } from '../data/addition';
 import { mysql } from '../app';
 import { generateAccessToken } from '../modules/access';
@@ -9,40 +9,43 @@ import { create } from 'domain';
 
 const userRoute = express.Router();
 
-userRoute.post("/register", async (req:Request, res:Response) => {
+userRoute.post("/register", async (req: Request, res: Response) => {
   try {
-    let firstname:string = req.body.firstname
-    let lastname:string = req.body.lastname
-    let email:string = req.body.email
-    let username:string = req.body.username
-    let pass:string = req.body.password
-    let role:string = req.body.role
-    let company:string = req.body.companyid
+    let firstname: string = req.body.firstname
+    let lastname: string = req.body.lastname
+    let email: string = req.body.email
+    let username: string = req.body.username
+    let pass: string = req.body.password
+    let role: string = req.body.role
+    let company: string = req.body.companyid
 
     const password = await encryptPassword(pass)
     let results = await emailInUse(email);
     if (results === true) {
-      res.status(409).send("Email in Use");
-      return
+      return res.status(409).send("Email in Use");
     } else {
       const guid: string = uuid.v4();
-      await createNewUser(guid, firstname, lastname, email, username, password, role, company, async(data) => {
-        getUserByUsername(username, async (d) => {
-          if (d === undefined) {
-            res.status(400).send("User does not exist");
-            return
-          }
-          else {
-            console.log('---Generating Access Token')
-            const token = await generateAccessToken(email)
-            console.log(token)
-            console.log({'access':token, d })
-            return res.json({ 'accessToken': token, d })
-          }
+      try {
+        await createNewUser(guid, firstname, lastname, email, username, password, role, company, async (data) => {
+          getUserByUsername(username, async (d) => {
+            if (d === undefined) {
+              return res.status(400).send("User does not exist");
+
+            } else {
+              console.log('---Generating Access Token')
+              const token = await generateAccessToken(email)
+              console.log(token)
+              console.log({ 'access': token, d })
+              return res.json({ 'accessToken': token, d })
+            }
+          })
         })
-    })
+
+      } catch (err) {
+        console.log(err)
+      }
     }
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 })
@@ -60,31 +63,31 @@ userRoute.post("/register", async (req:Request, res:Response) => {
 */
 
 userRoute.post("/login", async (req: Request, res: Response) => {
-    let username: string = req.body.username
-    let password: string = req.body.password
+  let username: string = req.body.username
+  let password: string = req.body.password
 
-    getUserByUsername(username, async (data) => {
-        if(!data) return res.status(400).send("User does not exist")
-        let check = await isPasswordCorrect(password, data.password)
-        if(!check) return res.status(400).send("Incorrect password")
-        // Give token to user
-        // Give user data + company to the client
+  getUserByUsername(username, async (data) => {
+    if (!data) return res.status(400).send("User does not exist")
+    let check = await isPasswordCorrect(password, data.password)
+    if (!check) return res.status(400).send("Incorrect password")
+    // Give token to user
+    // Give user data + company to the client
 
-        return res.send(data)
-    })
-    
+    return res.send(data)
+  })
+
 })
 
 userRoute.get("/:userId", async (req: Request, res: Response) => {
-    let userId:string = req.params.userId
-    // 291622df-1ed7-41fd-bc41-ce8f0ab87331
-    try {
-        getUserById(userId, (data) => {
-          res.send(data)
-        })
-      } catch(err) {
-        res.send(err)
-      }
+  let userId: string = req.params.userId
+  // 291622df-1ed7-41fd-bc41-ce8f0ab87331
+  try {
+    getUserById(userId, (data) => {
+      res.send(data)
+    })
+  } catch (err) {
+    res.send(err)
+  }
 })
 
-export {userRoute}
+export { userRoute }
